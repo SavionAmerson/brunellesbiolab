8c2844fc0ca5dea549ba55047aaef6566f148c77
SavionCychet <enochcychet@gmail.com>
2020-05-22 13:05:46 -0500

    Added images to topic section

--- src/components/topic-section.js
+++ src/components/topic-section.js
@@ -8,16 +8,19 @@ class Topics extends React.Component {
               <div className="topic-holder">
                 <div className="dna">
                   <h1>DNA MANIPULATION</h1>
+                  <img className="dna-graphic" src={require("../images/DNA.png")} />
                   <h2>You'll get to see how we maniupulate genetic material to create and modify
                     lifeforms!
                   </h2>
                 </div>
                 <div className="bacteria">
                 <h1>BACTERIA</h1>
+                <img className="bacteria-graphic" src={require("../images/Bacteria.png")} />
                 <h2>You'll get to observe different a bunch types of bacteria under a microscope!</h2>
                 </div>
                 <div className="cryogenics">
                 <h1>CRYOGENICS</h1>
+                <img className="ice-graphic" src={require("../images/Ice cube.png")} />
                 <h2>You'll get to learn about perserving frozen organs (and see them too)!</h2>
                 </div>
               </div>
